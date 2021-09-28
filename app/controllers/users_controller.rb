009f00d07aa5fc8edbe1a08e610d49cd807cd84f
ngkfndkngd <huku.0033@gmail.com>
2021-09-28 13:54:13 +0000

    [Add] 編集画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,17 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @book = @user.books
+    @book_new = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
   
   def user_params
