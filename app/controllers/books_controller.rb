ef1dec452797e7ad4e048ecd318e03d91ba6909e
ngkfndkngd <huku.0033@gmail.com>
2021-09-28 07:44:36 +0000

    [Add] user一覧画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,34 @@
 class BooksController < ApplicationController
+  
   def index
     @user=current_user
-    
+    @book=Book.new
+    @books=Book.all
+
   end
-  
+
+  def create
+    @user=current_user
+    @book = Book.new(book_params)
+    @books=Book.all
+    @book.user_id = current_user.id
+    @book.save
+      redirect_to book_path(@book.id)
+      
+      
+  end
+
   def edit
   end
-  
+
   def show
-  end  
+    
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
+    @users=User.all
+    @book = Book.new
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = @user.books
   end
 
   def edit
+  end
+  
+  def user_params
+    params.require(:user).permit(:name,:profile_image,:introduction)
   end
 end
